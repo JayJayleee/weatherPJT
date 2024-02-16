291fb879f5390aaeb5867a351b8f2f3a6612fbf7
JiwonShin <acd0825@naver.com>
2024-02-16 18:43:19 +0900

    Add: #1  클라우드 function 내 api 요청 내용 수정

--- src/components/dailyWeather/index.js
+++ src/components/dailyWeather/index.js
@@ -1,26 +1,24 @@
 import { firestore } from "../../firebase-config.js";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchWeather = async () => {
+  const weatherInfo = await firestore.collection("weatherInfo").doc("weather_item").get();
+  return weatherInfo.data();
+};
 
 export default function DailyWeatherComponent(){
     const navigate = useNavigate();
-    const [weather, setWeather] = useState({degree:0, status:""});
-    useEffect(() => {
-  
-      const bucket = firestore.collection("weatherInfo");
-  
-      bucket
-        .doc("weather_item")
-        .get()
-        .then((doc) => {
-          setWeather({
-            degree: doc.data().degree,
-            status: doc.data().status,
-          })
-        });
-    })
+    const { data: weatherdata, isLoading, error } = useQuery({
+      queryKey: ['getWeather'],
+      queryFn: fetchWeather,
+    });
+
+    if (isLoading) return <div>로딩중(로티같은거 넣기)</div>;
+    if (error) return <div>outfitlistpage 내 리액트 쿼리 중 getWeather 에러 발생 {error.message}</div>;
 
     return <div className="App">
-        <p>서울은 {weather.degree}도이고 날씨는 {weather.status} </p>
+        <p>서울은 {weatherdata.degree}도이고 날씨는 {weatherdata.status} </p>
         <button onClick={()=>navigate("/pickoutfit") }>이 날씨에 맞는 옷 찾기</button></div>;
 }
